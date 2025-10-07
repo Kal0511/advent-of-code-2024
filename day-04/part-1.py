f23cde26bf6cc041a30c31971d5bacbd76ec2032
Kaumil Patel <79810862+Kal0511@users.noreply.github.com>
2025-10-07 15:20:20 -0600

    day 4 completed

---
+++ day-04/part-1.py
@@ -0,0 +1,64 @@
+# Day 4: Ceres Search Part 2
+# https://adventofcode.com/2024/day/4
+# Time Complexity O(n)
+# Space Complexity O(n)
+
+
+def count_xmas(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    word = ['X', 'M', 'A', 'S']
+    word_length = len(word)
+    directions = [
+        (0, 1),  # Up
+        (0, -1),  # Down
+        (-1, 0),  # Left
+        (1, 0),  # Right
+        (1, 1),  # Up Right
+        (1, -1),  # Down Right
+        (-1, -1),  # Down Left
+        (-1, 1)  # Up Left
+    ]
+
+    def checker(x, y, dx, dy):
+        for i in range(1, word_length):
+            newX = x + i * dx
+            newY = y + i * dy
+
+            # Check if not out of bounds
+            if not ((0 <= newX < rows) and (0 <= newY < cols)):
+                return False
+
+            # Check if char doesnt match
+            if grid[newX][newY] != word[i]:
+                return False
+
+        return True
+
+    counter = 0
+    for x in range(rows):
+        for y in range(cols):
+            if grid[x][y] != 'X':
+                continue
+            for dx, dy in directions:
+                if checker(x, y, dx, dy):
+                    counter += 1
+    return counter
+
+
+def load_input():
+    grid = []
+    with open("part-1-input.txt", "r") as file:
+        for line in file:
+            grid.append(list(line.strip()))
+    return grid
+
+
+def main():
+    grid = load_input()
+    count = count_xmas(grid)
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day-04/part-2.py
@@ -0,0 +1,53 @@
+# Day 4: Ceres Search Part 2
+# https://adventofcode.com/2024/day/4
+# Time Complexity O(n)
+# Space Complexity O(n)
+
+def count_x_mas(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def checker(x, y):
+        part1 = False
+        part2 = False
+
+        # Top right to bottom left
+        if grid[x - 1][y - 1] == "M" and grid[x + 1][y + 1] == "S":
+            part1 = True
+        elif grid[x - 1][y - 1] == "S" and grid[x + 1][y + 1] == "M":
+            part1 = True
+
+        # Top left to bottom right
+        if grid[x - 1][y + 1] == "M" and grid[x + 1][y - 1] == "S":
+            part2 = True
+        elif grid[x - 1][y + 1] == "S" and grid[x + 1][y - 1] == "M":
+            part2 = True
+
+        return part1 and part2
+
+    counter = 0
+    for x in range(1, rows - 1):
+        for y in range(1, cols - 1):
+            if grid[x][y] != 'A':
+                continue
+            if checker(x, y):
+                counter += 1
+    return counter
+
+
+def load_input():
+    grid = []
+    with open("part-2-input.txt", "r") as file:
+        for line in file:
+            grid.append(list(line.strip()))
+    return grid
+
+
+def main():
+    grid = load_input()
+    count = count_x_mas(grid)
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
